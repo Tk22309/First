6404a649376b2c14b74db1808254c4793a0a183e
Tk22309 <129153902+Tk22309@users.noreply.github.com>
2023-05-15 23:16:30 +0300

    Add files via upload

---
+++ lab15/1.js
@@ -0,0 +1,12 @@
+
+  
+  function getDescription(num) {
+    if (num = 0) {
+      return "це нуль";
+    } else if (num % 2 = 0) {
+      return "парне число";
+    } else {
+      return "непарне число";
+    }
+  }
+  
